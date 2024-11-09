127194b91d0b4d55c5034935457565737ed90b0b
Alvaro Luque Morillo <aluqmor651@ieszaidinvergeles.org>
2024-11-09 11:44:26 +0100

    Cambios json para llamar a los elementos

--- server/interfaces/model_1.js
+++ server/interfaces/model_1.js
@@ -1,18 +1,35 @@
 export const model1 = {
     panels: [
         {
-            type: 'panel',
-            panelName: 'panel1',
-            panelDescription: 'panel1 description',
-            panelWidth: '100',
-            panelHeight: '100',
-            cssData: 'panel_style_1'
+            name: 'panel1',
+            width: '100',
+            height: '100',
+            cssdata: 'panel_style_1'
+        },
+        {
+            name: 'panel2',
+            width: '100',
+            height: '100',
+            cssdata: 'panel_style_2'
         }
     ],
     components: [
         {
-            type: 'boton',
-            content: 'Pulsa aquí',
+            type: 'div',
+            name: 'div_panel1_1',
+            father: 'panel1',
+            position: '0,0'
+        },
+        {
+            type: 'div',
+            name: 'div_panel2_1',
+            father: 'panel2',
+            position: '0,0'
+        },
+        {
+            type: 'div',
+            name: 'div_panel2_2',
+            father: 'panel2',
             position: '0,0'
         }
     ]
