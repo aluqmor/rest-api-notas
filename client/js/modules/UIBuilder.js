24b562869d079157fce8a2df4d79e44a9d9e31dd
Alvaro Luque Morillo <aluqmor651@ieszaidinvergeles.org>
2024-11-09 13:23:22 +0100

    Cambio Interfaces

--- client/js/modules/UIBuilder.js
+++ client/js/modules/UIBuilder.js
@@ -3,9 +3,11 @@ import { ComponentMapper } from "../mapper/ComponentMapper.js";
 
 export class UIBuilder {
     static build(data) {
-        console.log(data.panels);
+        const opciones = document.getElementById('opciones');
+        let value = '';
         const contenedor = document.getElementById("main_container");
 
+        console.log(data.panels);
         data.panels.forEach(element => {
             const div = PanelMapper(element);
             contenedor.appendChild(div);
@@ -13,6 +15,17 @@ export class UIBuilder {
 
         data.components.forEach(element => {
             ComponentMapper(element);
+        });
+
+        opciones.addEventListener('change', (event) => {
+            const selectedValue = event.target.value;
+            if (selectedValue === 'opcion1') {
+                const panel2 = document.getElementById(panel2);
+                panel2.remove();
+            } else if (selectedValue === 'opcion2') {
+                const panel1 = document.getElementById(panel1);
+                panel1.remove();
+            }
         });
     }
 }
